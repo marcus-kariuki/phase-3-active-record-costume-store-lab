f6880f2e0da12408e0d2c1037c931766cd26b27a
Marcus <marcus.kariuki@student.moringaschool.com>
2023-02-26 22:49:54 +0300

    completed

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -1,2 +1,15 @@
 # Create your Costume class here
 # It should inherit from ActiveRecord::Base
+
+class CreateCostumes < ActiveRecord::Migration[6.1]
+    def change
+      create_table :costumes do |t|
+        t.string :name
+        t.float :price 
+        t.string :size
+        t.string :image_url
+        t.timestamps
+    end
+  end
+  end
+  
